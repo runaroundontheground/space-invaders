b4e0e78252d9c708d679b29b2081d3ec4761a7e6
runaroundontheground <118295064+runaroundontheground@users.noreply.github.com>
2023-01-24 09:19:24 -0700

    its the game but probably working now

---
+++ space invaders/game.py
@@ -0,0 +1,16 @@
+#importing things
+import random
+import time
+import turtle
+
+from turtle import *
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
+
