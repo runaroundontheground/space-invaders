a3c300974f3ab04e8a47fece3db955871477562a
runaroundontheground <118295064+runaroundontheground@users.noreply.github.com>
2023-01-26 11:14:55 -0700

    update it

--- space invaders/game.py
+++ space invaders/game.py
@@ -2,15 +2,72 @@
 import random
 import time
 import turtle
+import tkinter
+import howdo it do this??
+from PIL.Image import core as _imaging
+from tkinter import *
+from tkinter import ttk
+#variables go here
+#s = turtle.getscreen();
+#t = turtle.Turtle();
 
-s = turtle.getscreen();
-t = turtle.Turtle();
-big1 = "images/big1.gif";
-big2 = "images/big2.gif";
-turtle.addshape(big1);
-turtle.addshape(big2);
-while True:
-    t.shape(big1)
-    time.sleep(0.1);
-    t.shape(big2);
-    time.sleep(0.1);
+#font = "font/PressStart2P-Regular";
+#style = (font, 50); #font name, font size, font type
+#big1 = Image.open("images/big1green.gif");
+#big2 = "images/big2.gif";
+#big1.resize();
+
+#turtle.addshape(big1);
+#turtle.bgcolor("black");
+
+#messing with tkinter
+root = tkinter.Tk();
+root.geometry("375x200");
+
+
+
+
+
+
+gameState = "menu"; # can be "menu", "game", or "gameover"
+
+
+
+
+
+
+
+#t.setpos(-100, 100);
+#menu things
+#while gameState == "game":
+    #t.write("SPACE INVADERS", font = style);
+    #t.write("START", font = style);
+    #t.write("OPTIONS", font = style);
+
+
+
+
+
+
+
+
+
+
+
+#actual game
+#while gameState == "menu":
+    #t.shape(big1)
+    #time.sleep(0.01);
+    #t.shape(big2);
+    #t.write("Press Start", font = style);
+    #time.sleep(0.01);
+
+
+
+
+
+
+#game over
+
+#stuff
+
--- space invaders/menu.py
+++
@@ -1,7 +0,0 @@
-#Menu Stuff
-import turtle
-import time
-
-a = 1
-if a == 1:
-    print("SPACE INVADERS")
