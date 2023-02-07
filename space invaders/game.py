c89452d5e83ecd2f05ecaff3b68167a3cef9e184
runaroundontheground <118295064+runaroundontheground@users.noreply.github.com>
2023-02-07 10:33:10 -0700

    update

--- space invaders/game.py
+++ space invaders/game.py
@@ -3,8 +3,13 @@ import math
 import random
 import time
 import pygame
+import os
 #variables go here
-Font = pygame.font.Font("PressStart2P.ttf", 64)
+pygame.font.init();
+pygame.mixer.init();
+pygame.init();
+clock = pygame.time.Clock();
+Font = pygame.font.Font("font/PressStart2P-Regular.ttf", 64)
 
 from pygame.locals import (
     K_UP,
@@ -20,48 +25,77 @@ gameState = "menu"; # can be "menu", "game", or "gameover"
 sW, sH = 800, 600;
 screen = pygame.display.set_mode((sW, sH));
 
+#Colors
+RED = (255, 0, 0)
+BLUE = (0, 255, 0)
+GREEN = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+FPS = 60;
+spd = 3; #speed for player
+mxspd = 5; #player max speed
+bspd = 10; #projectile speed
+espd = 3; #enemy speed
+maxBullets = 1;
+plrBul = [];
+
+#images
+plrW, plrH = 32, 32;
+plrImg = pygame.image.load(os.path.join("images", "player.png" ));
+plrImg = pygame.transform.scale(plrImg, (plrW, plrH));
+plrRect = pygame.Rect(200, 200, plrW, plrH);
+plr = {
+    "x": 0.0,
+    "y": 0.0,
+    "xv": 0.0,
+    "yv": 0.0
+    };
+pBul = pygame.Rect(round(plr["x"]), round(plr["y"]), 5, 5);
+
+plr["xv"] = 1;
+
+def plrMove():
+    #player movement
+    global sW, sH, spd, pBul, bspd, mxspd;
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_d] and plr["x"] - spd  > 0: #go right
+        if plr["xv"] < mxspd:
+            plr["xv"] += spd;
+
+    if keys[pygame.K_a] and plr["x"] + spd + plrW  < sW: #go left
+       if plr["xv"] > -mxspd:
+        plr["xv"] -= spd;
+
+    if (not keys[pygame.K_d] and not keys[pygame.K_a]) or (keys[pygame.K_d] and keys):
+        plr["xv"] -= plr["xv"] / 5;
+
+    if plr["x"] < 0:
+        plr["xv"] = 1.0;
+
+    if plr["x"] > sW:
+        plr["xv"] = -1.0;
+
+    if keys[pygame.K_SPACE] and len(pBul) < maxBullets: #shoot a bullet if there's not a playr bullet alread on screen
+        #pBul = pygame.Rect(plrRect.x + plrW, plr.y + plrH/2 -2, 10, 5) 
+        pBul.append(plrBul)
+
+    if pBul.y > 0: #if bullet y is not off top of screen
+        pBul.y += bspd
+
+
+bigImg = pygame.image.load(os.path.join("images", "big.png"))   
+bigW, bigH = 32, 32;
+big = pygame.transform.scale(bigImg, (bigW, bigH))
+bigRect = pygame.Rect(100, 100, bigW, bigH);     
 
 
 
+"""
+def bCollision(pBul, eBul, plrRect, bigRect, smallRect): #still need to define eBul, and smallRect
 
 
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Player, self).__init__()
-        self.surf = pygame.image.load("images/player.png").convert();
-        #self.surf.set_colorkey((255, 255, 255), RLEACCEL); whats this do
-        self.rect = self.surf.get_rect();
-        
-
-
-
-    def update(self, Pkeys):
-        if Pkeys[K_UP]:
-            self.rect.move_ip(0, -5);
-        if Pkeys[K_DOWN]:
-            pass
-
-
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Enemy, self).__init__();
-        self.surf = pygame.image.load("images/big.png").convert();
-        #thing above again, fixes something?
-
-
-class BG(pygame.sprite.Sprite):
-    def __init__(self):
-        super(BG, self).__init__();
-        self.surf = pygame.image.load("images/bg.png").convert();
-        
-        
-           
-pygame.mixer.init();
-pygame.init();
-clock = pygame.time.Clock();
-
+"""
 
 
 
@@ -70,140 +104,53 @@ clock = pygame.time.Clock();
 #menu things
 
 def menu():
-    menu_text = Font.render("SPACE INVADERS", True, (0, 255, 255))
-    screen.blit(menu_text, (190, 250))
+    menuText = Font.render("SPACE INVADERS", True, GREEN, BLUE)
+    menuTextRect = menuText.get_rect();
+    screen.blit(menuText, menuTextRect)
+
     
 
-while gameState == "menu":
-    menu()
+
 
 #input thingys
-def onClick(x = 0, y = 0):
-    global mx, my, gameState
-    #mD = True;
-    print(str(x) + ", " + str(y));
-    if mx > -84 and mx < 19 and my > -72 and my < -50:
-        print("start clicked");
-        #gameState = "game";
 
-def mouseMove(event):
-    global mx, my, sW, sH
-    mx, my = event.x, event.y;
-    print(mx - (sW / 2));
+
     
 
 
 
-
+def screenThings():
+    screen.fill(RED) # reset window maybe
+    # render things
+    plr["x"] += plr["xv"];
+    plr["y"] += plr["yv"];
+    screen.blit(plrImg, (round(plr["x"]), round(plr["y"])));
+    #for length of enemies:
+    #screen.blit(enemies[i], enemies[i]but get x)
+    #screen.blit()
+    # update positions?
+    pygame.display.update();
 
 #this is where we call things, don't assign values / functions below here
 
-
-
-
-while gameState == "game":
-    pass
-
+pygame.display.set_caption("AAAAA");
+running = True;
+while running:
+    screenThings();
+    plrMove();
+    clock.tick(FPS);
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False;
+    if gameState == "menu":
+        menu();
+    if gameState == "game":
+        pass
+    
+pygame.quit();
 #while gameState == "gameover":
 #   pass
 
 
 
 
-
-
-
-
-
-
-"""
- 
-#actual game
-while gameState == "game":
-    t.shape(big1);
-    time.sleep(0.01);
-    t.write("Press Start", font = style);
-    time.sleep(0.01);
-
-def drawBorder():
-    border_pen.color=("black")
-    border_pen.pensize=(5)
-    border_pen.speed=(0)
-    border_pen.penup
-    border_pen.setpos(500,400)
-    border_pen.pendown
-    for side in range(4):
-        border_pen.fd(1000)
-        border_pen.lt(800)
-    border_pen.hideturtle()
-
-#defining player
-player= turtle.Turtle
-player.shape(playerImg)
-player.speed(0)
-player.penup
-player.setpos(0,-350)
-player.setheading(90)
-playerSpeed = 10
-
-#defining bullet
-def bullet:
-    bullet = turtle.Turtle
-    bullet.shape("bullet.gif")
-    bullet.speed(0)
-    bullet.penup
-    bullet.setpos(player.pos, -340)
-    bullet.setheading(90)
-    bulletSpeed = 20
-
-#player movement
-turtle.onkeypress("a")
-while turtle.onkeypress("a") = true:
-    player.setpos -= 10
-    if player.position = -500:
-        break
-
-turtle.onkeypress("d")
-while turtle.onkeypress("d") = true:
-    player.setpos += 10
-    if player.position = 500:
-        break
-turtle.onkeypress("space")
-if turtle.onkeypress("space") = true:
-    bullet
-
-#defining small enemy
-#some how need to spawn 16 of then in different locations
-smallEnemy = turtle.Turtle
-smallEnemy.shape("smallGuy.gif")
-smallEnemy.speed(0)
-smallEnemy.setpos("0,330")
-smallEnemy.setheading(90)
-smallEnemySpeed = 5
-
-#defining big enemy
-#some how need to spawn 8 of then in different locations
-bigEnemy = turtle.Turtle
-bigEnemy.shape(Big1)
-bigEnemy.speed(0)
-bigEnemy.setpos("0, 280")
-bigEnemy.setheading(90)
-bigEnemySpeed = 5
-
-
-#defining motherShip
-if num = 6:
-    motherShip = turtle.Turtle
-    motherShip.shape("mothership.gif")
-    motherShip.speed(0)
-    motherShip.setpos("-500, 390")
-    motherShip.setheading(90)
-
-
-"""
-
-#game over
-aaa = input("type things");
-print(aaa);
-#stuff
-#s().bye();
