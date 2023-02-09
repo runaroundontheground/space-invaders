20b71fac5408e70640a2676a10fc9a88c06b6cc2
runaroundontheground <118295064+runaroundontheground@users.noreply.github.com>
2023-02-09 10:31:35 -0700

    update

--- space invaders/game.py
+++ space invaders/game.py
@@ -9,7 +9,7 @@ pygame.font.init();
 pygame.mixer.init();
 pygame.init();
 clock = pygame.time.Clock();
-Font = pygame.font.Font("font/PressStart2P-Regular.ttf", 64)
+font = pygame.font.Font("font/PressStart2P-Regular.ttf", 64)
 
 from pygame.locals import (
     K_UP,
@@ -27,26 +27,30 @@ screen = pygame.display.set_mode((sW, sH));
 
 #Colors
 RED = (255, 0, 0)
-BLUE = (0, 255, 0)
-GREEN = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 FPS = 60;
-spd = 3; #speed for player
-mxspd = 5; #player max speed
+
+PLAYERSHIP = pygame.image.load(os.path.join("images", "player.png"));
+ALIEN1 = pygame.image.load(os.path.join("images", "alien1.png"))
+#ALIEN2 = pygame.image.load(os.path.join("images", "alien2.png"))
+spd = 1; #speed for player
+mxspd = 3; #player max speed
 bspd = 10; #projectile speed
 espd = 3; #enemy speed
 maxBullets = 1;
 plrBul = [];
-
+text = [];
 #images
 plrW, plrH = 32, 32;
-plrImg = pygame.image.load(os.path.join("images", "player.png" ));
+plrImg = PLAYERSHIP;
 plrImg = pygame.transform.scale(plrImg, (plrW, plrH));
 plrRect = pygame.Rect(200, 200, plrW, plrH);
 plr = {
-    "x": 0.0,
-    "y": 0.0,
+    "x": (sW / 2) - plrW,
+    "y": sH - plrH,
     "xv": 0.0,
     "yv": 0.0
     };
@@ -59,60 +63,98 @@ def plrMove():
     global sW, sH, spd, pBul, bspd, mxspd;
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_d] and plr["x"] - spd  > 0: #go right
+    if keys[pygame.K_d] and plr["x"] > 0: #go right
         if plr["xv"] < mxspd:
             plr["xv"] += spd;
 
-    if keys[pygame.K_a] and plr["x"] + spd + plrW  < sW: #go left
+    if keys[pygame.K_a] and plr["x"] + plrW  < sW: #go left
        if plr["xv"] > -mxspd:
         plr["xv"] -= spd;
+    
+    if keys[pygame.K_w] and plr["y"] > sH - 200: # go up
+        if plr["yv"] > -mxspd:
+            pass
 
     if (not keys[pygame.K_d] and not keys[pygame.K_a]) or (keys[pygame.K_d] and keys[pygame.K_a]):
         plr["xv"] -= plr["xv"] / 5;
 
     if plr["x"] < 0:
-        plr["xv"] = 1.0;
+        plr["x"] += 1
+        plr["xv"] = 0.0;
+        
 
     if plr["x"] > sW:
-        plr["xv"] = -1.0;
-
-    if keys[pygame.K_SPACE] and len(pBul) < maxBullets: #shoot a bullet if there's not a playr bullet alread on screen
+        plr["x"] -= 1
+        plr["xv"] = 0.0;
+    if keys[pygame.K_SPACE] and len(plrBul) < maxBullets: #shoot a bullet if there's not a playr bullet alread on screen
         #pBul = pygame.Rect(plrRect.x + plrW, plr.y + plrH/2 -2, 10, 5) 
         pBul.append(plrBul)
-
+    
+    
     if pBul.y > 0: #if bullet y is not off top of screen
         pBul.y += bspd
+    #else
 
 
-bigImg = pygame.image.load(os.path.join("images", "big.png"))   
-bigW, bigH = 32, 32;
-big = pygame.transform.scale(bigImg, (bigW, bigH))
-bigRect = pygame.Rect(100, 100, bigW, bigH);
+Alien1Count = 0;
+Alien1xpos =100;
+Alien1ypos = 100;
 
-
+def alien1():
+    
+    alien1Img = ALIEN1; 
+    alien1W, alien1H = 32, 32;
+    alien1 = pygame.transform.scale(alien1Img, (alien1W, alien1H))
+    while Alien1Count < 8:
+        alien1Rect = pygame.Rect(Alien1xPos, Alien1yPos, alien1W, alien1H);
+        alien1xpos += 3
+        Alien1Count += 1
+        if Alien1Count < 8:
+            break
+        
+    
+"""
+plrAlive = True
+while plrAlive:
+    while alien1xpos < 790:
+        clock.tick(FPS);
+        alien1xpos += 6
+    while alien1xPos > 10:
+        clock.tick(FPS);
+        alien1xpos -= 6
 
 """
-def bCollision(pBul, eBul, plrRect, bigRect, smallRect): #still need to define eBul, and smallRect
 
 
-"""
 
 #collision is already handled by pygame, don't do anything about it for now
 
 
 
 #menu things
-
+def makeMenu():
+    menuText = {
+        "0": {
+            "text": font.render("Space Invaders", True, BLACK),
+            "pos": (100, 100)
+            },
+        "1": {
+            "text":font.render("Start", False, BLACK),
+            "pos": (296, 274)
+            }
+    };
+    
+    text.append(menuText);
+    
 def menu():
-    menuText = Font.render("SPACE INVADERS", True, GREEN, BLUE)
-    menuTextRect = menuText.get_rect();
-    screen.blit(menuText, menuTextRect)
-
+    pass
+    
+    
     
 
 
 
-#input thingys
+
 
 
     
@@ -120,20 +162,34 @@ def menu():
 
 
 def screenThings():
-    screen.fill(RED) # reset window maybe
+    screen.fill(GREEN) # reset window maybe
     # render things
     plr["x"] += plr["xv"];
     plr["y"] += plr["yv"];
     screen.blit(plrImg, (round(plr["x"]), round(plr["y"])));
+    
+    i = 0;
+    while i < len(text):
+        screen.blit(text[i][str(i)]["text"], text[i][str(i)]["pos"])
+        i += 1;
+    
+    # show mouse position
+    mpos1 = font.render(str(pygame.mouse.get_pos()[0]) + " " + str(pygame.mouse.get_pos()[1]), False, BLUE)
+    mpos1W = int(mpos1.get_size()[0] / 6);
+    mpos1H = int(mpos1.get_size()[1] / 6);
+    mpos1 = pygame.transform.scale(mpos1, (mpos1W, mpos1H))
+    screen.blit(mpos1, (pygame.mouse.get_pos()[0] + 30, pygame.mouse.get_pos()[1]))
     #for length of enemies:
     #screen.blit(enemies[i], enemies[i]but get x)
     #screen.blit()
     # update positions?
-    pygame.display.update();
+    pygame.display.flip();
 
 #this is where we call things, don't assign values / functions below here
 
-pygame.display.set_caption("AAAAA");
+makeMenu();
+print(len(text));
+pygame.display.set_caption("Frijoles Con Limon");
 running = True;
 while running:
     screenThings();
@@ -142,6 +198,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False;
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos());
     if gameState == "menu":
         menu();
     if gameState == "game":
@@ -154,3 +212,16 @@ pygame.quit();
 
 
 
+'''
+def main_menu():
+    title_font = Font
+    run = True
+    while run:
+        screen.blit(BLACK, (0,0))
+        title_text = title_font.render("Click to Begin", 1, GREEN)
+        screen.blit(title_text, (sW/2 - title_text.get_width()/2, 350))
+        pygame.display.update()
+        for event in pygame.event.get():
+
+
+'''
